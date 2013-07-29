990865ef92cbbdbac25cab9b5f8312fca49cca11
Branden Visser <mrvisser@gmail.com>
2013-07-28 21:59:03 -0400

    Fixing invalid reference to lib/util.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
 module.exports.context = require('./lib/context');
 module.exports.logger = require('./lib/logger');
 module.exports.redis = require('./lib/redis');
-module.exports.util = require('./lib/util');
